5c7938a9e9760c91ddb815be591295d83428b9ed
jessica stenning <jessicastenning@hotmail.co.uk>
2018-04-15 16:11:54 +0100

    Puts names beginning with 'b' only

--- directory.rb
+++ directory.rb
@@ -24,7 +24,8 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
-  puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if
+  student[:name].chr.downcase == "b"
   end
 end
 
