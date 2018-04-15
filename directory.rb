4ced83f5df58ce1a630f2b78641d33e4860e7e10
jessica stenning <jessicastenning@hotmail.co.uk>
2018-04-15 16:25:55 +0100

    Puts names less than 12 characters long

--- directory.rb
+++ directory.rb
@@ -23,9 +23,9 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student, index|
-  puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if
-  student[:name].chr.downcase == "b"
+  students.each do |student|
+  puts "#{student[:name]} (#{student[:cohort]} cohort)" if
+  student[:name].length < 12
   end
 end
 
